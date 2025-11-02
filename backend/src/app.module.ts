7aa2fd9789b20eb1c5d14d78f7f2dbb141f34fdf
devojeda <172947644+DevOjeda016@users.noreply.github.com>
2025-11-01 21:59:25 -0600

    feat (back): add database module with TypeORM configuration and environment-based settings
    - Integrated ConfigModule for global configuration management.
    - Created DatabaseModule to handle TypeORM setup with PostgreSQL.
    - Configured database connection parameters using environment variables.
    - Enabled dynamic synchronization based on environment settings.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: config.get('TYPEORM_SYNCHRONIZE') === 'true', // dinámico según entorno
+        migrations: ['dist/database/migrations/*.js'],
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
