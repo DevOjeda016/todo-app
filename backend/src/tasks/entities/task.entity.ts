0a63e7f81606367ef779e60cd267235f3118e78a
devojeda <172947644+DevOjeda016@users.noreply.github.com>
2025-11-01 22:51:27 -0600

    feat (back): update Task entity to use correct composite index and add Expose decorators

--- backend/src/tasks/entities/task.entity.ts
+++ backend/src/tasks/entities/task.entity.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import {
   Column,
   CreateDateColumn,
@@ -11,14 +12,17 @@ import { Importance } from '../enums/importance.enums';
 import { Status } from '../enums/status.enums';
 
 @Entity('tasks')
-@Index(['completed', 'importance']) // Composite index for completed and importance fields
+@Index(['status', 'importance']) // Composite index for completed and importance fields
 export class Task {
+  @Expose()
   @PrimaryGeneratedColumn('uuid')
   id: string; // Unique identifier (UUID) for the task
 
+  @Expose()
   @Column({ length: 200 })
   title: string; // Short title or name of the task
 
+  @Expose()
   @Column({
     type: 'enum',
     enum: Importance,
@@ -26,6 +30,7 @@ export class Task {
   })
   importance: Importance; // Task importance level (enum)
 
+  @Expose()
   @Column({
     type: 'enum',
     enum: Status,
@@ -33,24 +38,31 @@ export class Task {
   })
   status: Status; // Task status (enum)
 
+  @Expose()
   @Column({ type: 'text', nullable: true })
   description?: string; // Detailed description of the task (optional)
 
+  @Expose()
   @Column({ type: 'timestamp', nullable: true })
   dueDate?: Date; // Optional due date and time for the task
 
+  @Expose()
   @Column({ type: 'int', nullable: true })
   estimatedMinutes?: number; // Estimated time to complete the task in minutes (optional)
 
+  @Expose()
   @Column('simple-array', { nullable: true })
   tags?: string[]; // Simple array of tags to categorize the task (optional)
 
+  @Expose()
   @Column({ default: false })
   archived: boolean; // Whether the task is archived or not
 
+  @Expose()
   @CreateDateColumn()
   createdAt: Date; // Timestamp of when the task was created
 
+  @Expose()
   @UpdateDateColumn()
   updatedAt: Date; // Timestamp of the last update to the task
 
