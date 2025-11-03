9e9d934d1545d39d5418c8692d04bf5befe1498b
devojeda <172947644+DevOjeda016@users.noreply.github.com>
2025-11-03 14:35:59 -0600

    feat(back): configure CORS to use dynamic frontend URL from environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200', // o "*" en desarrollo
+    origin: process.env.URL_FRONTEND,
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true,
   });
